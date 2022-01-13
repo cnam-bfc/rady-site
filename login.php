ed5d32f2de26f04a0db16a8167b5cd3c293706e2
FarmVivi <farmvividev@gmail.com>
2022-01-13 22:08:01 +0100

    better
    Co-authored-by: albanrouault <albanrouault@users.noreply.github.com>

--- login.php
+++ login.php
@@ -12,20 +12,20 @@
 
 <body>
 
-    <div id="login_container">
+    <div id="auth_main_div">
 
-        <form action="submit_login.php" method="POST" id="login_form">
+        <form action="submit_login.php" method="POST" id="auth_form">
 
             <h1>Connexion</h1></br>
 
-            <label for="email" id="login_id">Identifiant</label>
-            <input type="email" name="email" id="login_email" autofocus required placeholder="Email / pseudo" /></br>
+            <label for="email">Identifiant</label>
+            <input type="email" name="email" autofocus required placeholder="Email / pseudo" /></br>
 
             <label for="password">Mot de passe</label>
-            <input type="password" name="password" id="login_password" required placeholder="Mot de passe" />
+            <input type="password" name="password" required placeholder="Mot de passe" />
 
-            <div id="login_container2">
-                <input type="submit" value="Se connecter" class="auth_buttom"/></br>
+            <div id="auth_button_div">
+                <input type="submit" value="Se connecter" id="auth_buttom"/></br>
                 <a href="forgot_password.php">Mot de passe oublié?</a></br>
                 <a href="register.php">Pas encore inscrit?</a>
             </div>
