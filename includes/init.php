d1c07adbcd5d3ce3e06d2b38b016b121b0c0d14a
FarmVivi <farmvividev@gmail.com>
2022-07-02 02:09:43 +0200

    Database IP update

--- includes/init.php
+++ includes/init.php
@@ -6,7 +6,7 @@ session_start();
 <?php
 // Connexion à la base de données
 try {
-    $mysqlClient = new PDO('mysql:host=10.254.1.3;dbname=rady;charset=utf8', 'rady', 'yWEFf3ioTM9BHmAa');
+    $mysqlClient = new PDO('mysql:host=10.254.0.10;dbname=rady;charset=utf8', 'rady', 'yWEFf3ioTM9BHmAa');
 } catch (Exception $e) {
     $_SESSION['ERROR_MSG'] = 'Erreur de connexion à la base de données:</br>' . $e->getMessage();
     include_once('includes/error.php');
